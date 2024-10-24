a9a13b49e0a6cf1b3a1aa0325ae4d0103f2e0d59
dayvid <dayvidcristianosilva@gmail.com>
2024-10-23 21:21:02 -0300

    function

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Olá, mundo!")
+function somar(a, b) {
+  let soma = a + b
+  return soma
+}
+console.log("Soma: " + somar(2, 3))
+
+function multi(a, b) {
+  let multip = a * b
+  return multip
+}
+console.log("Multiplicação: " + multi(2, 3))
+
+function subt(a, b) {
+  let subtr = a - b
+  return subtr
+}
+console.log("Subtração: " + subt(10, 2))
+
+function div(a, b) {
+  let divi = a / b
+  return divi
+}
